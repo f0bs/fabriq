319d02aabef6c561c3994f4255489bf1a373fc32
Hwee Lin Yeo <hweelinyeo@Hwees-MacBook-Pro.local>
2020-04-10 19:30:03 -0400

    react native setup

---
+++ ios/Pods/Target Support Files/Pods-fabriq-fabriqTests/Pods-fabriq-fabriqTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_fabriq_fabriqTests : NSObject
+@end
+@implementation PodsDummy_Pods_fabriq_fabriqTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-fabriq-tvOSTests/Pods-fabriq-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_fabriq_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_fabriq_tvOSTests
+@end
