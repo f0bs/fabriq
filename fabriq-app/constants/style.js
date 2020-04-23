ab73d12b90e1fd0ead7f7e5126e92a260891a5f2
hweelin-yeo <hy384@cornell.edu>
2020-04-23 03:50:28 -0400

    Modified EmailsFound screen

--- fabriq-app/constants/style.js
+++ fabriq-app/constants/style.js
@@ -1,5 +1,8 @@
 // - MARK: Colors
 export const LIGHTBLUE = '#3FBAFF';
+export const GREEN = 'green';
 
 // - MARK: Size
 export const BUTTON_FONT_SIZE = 16;
+export const SMALL_BUTTON_FONT_SIZE = 10;
+
--- fabriq-app/screens/EmailsFound.js
+++ fabriq-app/screens/EmailsFound.js
@@ -1,50 +1,55 @@
 import React, { Component } from 'react';
-import { View, Text, SafeAreaView, FlatList, TouchableOpacity, ActivityIndicator, StyleSheet } from 'react-native';
+import { ScrollView, View, Text, SafeAreaView, FlatList, TouchableOpacity, ActivityIndicator, StyleSheet } from 'react-native';
 import { ListItem } from 'react-native-elements'
-
-const DATA_DUMMY = [
-    {
-      name: {
-          first: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-          last: 'bla'
-      },
-      email: 'hello@gmail.com'
-    },
-
-    {
-        name: {
-            first: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-            last: 'bla'
-        },
-        email: 'hello@gmail.com'
-      },
-
-      {
-        name: {
-            first: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-            last: 'bla'
-        },
-        email: 'hello@gmail.com'
-      },
-    
-  ];
+import * as FabriqStyle from '../constants/style.js';
 
 class ClothingList extends Component {
+
     render() {
+      const { clothing_data } = this.props;
         return (
+          <ScrollView>
             <FlatList
-        data={DATA_DUMMY}
+        data={clothing_data}
         renderItem={({ item }) => (
-          <ListItem
-            // roundAvatar
-            title={`${item.name.first} ${item.name.last}`}
-            subtitle={item.email}
-            // avatar={{ uri: item.picture.thumbnail }}
-          />
+          <ClothingCell item = {item}/>
         )}
+        ItemSeparatorComponent={this.renderSeparator}
+        keyExtractor={item => item.id}
       />
+      </ScrollView>
         )
     }
+
+    renderSeparator = () => {
+      return (
+        <View
+          style={{
+            height: 1,
+            width: "100%",
+            backgroundColor: "#CED0CE",
+            // marginLeft: "0%"
+          }}
+        />
+      );
+    };
+  
+    renderFooter = () => {
+      if (!this.state.loading) return null;
+  
+      return (
+        <View
+          style={{
+            paddingVertical: 20,
+            borderTopWidth: 1,
+            borderColor: "#CED0CE"
+          }}
+        >
+          <ActivityIndicator animating size="large" />
+        </View>
+      );
+    };
+  
 }
 
 function Item({ id, title }) {
@@ -61,18 +66,42 @@ function Item({ id, title }) {
     );
   }
 
+  class AddButton extends Component {
+    render() {
+      return(
+        <TouchableOpacity
+        onPress={() => {}}
+        style={clothingStyles.add_button_unselected}
+        >
+           <Text style={clothingStyles.add_button_text}>Add Item</Text>
+        </TouchableOpacity>
+      )
+    }
+  }
+
+
+
 class ClothingCell extends Component {
     render() {
-
+      const { item } = this.props;
+      return(
+      <ListItem
+            leftAvatar={{ uri: item.uri}}
+            title={`${item.name.full_name} \n${item.purchase_price} \n`}
+            titleNumberOfLines = {2}
+            titleStyle = {clothingStyles.title_style}
+            titleContainerStyle = {clothingStyles.title_container}
+            subtitle={`Purchase Date: ${item.purchase_date}`}
+            subtitleStyle = {clothingStyles.subtitle_style}
+            bottomDivider
+            containerStyle={{ borderBottomWidth: 0 }}
+            rightElement = {<AddButton />}
+          />
+      )
     }
 }
 
 export default class EmailsFound extends Component {
-    constructor(props) {
-        super(props);
-        console.log("Reached here");
-        console.log(props);
-    }
 
     render() {
       const { goBack, navigate } = this.props.navigation;
@@ -90,8 +119,8 @@ export default class EmailsFound extends Component {
           from your email account, please confirm: </Text>
         </View>
 
-        <View style = {styles.container}>
-          <ClothingList clothing_data = {["hello"]} />
+        <View style = {clothingStyles.clothinglist_container}>
+          <ClothingList clothing_data = {DATA_DUMMY} />
         </View>
 
         
@@ -101,6 +130,64 @@ export default class EmailsFound extends Component {
     )
   }
 }
+
+  const clothingStyles = StyleSheet.create({
+    clothinglist_container: {
+      marginLeft: '5%',
+      width: '90%',
+      marginTop: 10,
+      justifyContent: 'flex-start', 
+      alignContent: 'flex-start',
+      backgroundColor: 'white',
+      marginBottom: 10
+    },
+
+    add_button_unselected: {
+      marginTop: 15,
+      marginBottom: 5,
+      backgroundColor: FabriqStyle.LIGHTBLUE, //"#03adfc",
+      paddingHorizontal: 10,
+      paddingVertical: 5,
+      height: 30,
+      borderRadius: 5,
+      width: 90,
+      textAlign: 'center',
+    },
+
+    add_button_selected: {
+      marginTop: 15,
+      marginBottom: 5,
+      backgroundColor: FabriqStyle.GREEN, //"#03adfc",
+      paddingHorizontal: 10,
+      paddingVertical: 5,
+      height: 30,
+      borderRadius: 5,
+      width: 90,
+      textAlign: 'center',
+    },
+
+    add_button_text: {
+      fontSize: FabriqStyle.BUTTON_FONT_SIZE,
+      fontWeight: '700',
+      color: "white", // "#0384fc" //"white",
+    },
+
+    title_container: {
+      marginBottom: 0,
+      marginLeft: 5
+    },
+
+    title_style: {
+      color: 'black',
+      fontSize: 15,
+      fontFamily: 'Avenir-Medium'
+    },
+
+    subtitle_style: {
+      fontFamily: 'Avenir-Medium',
+      color: 'darkgray'
+    }
+  })
   
   const styles = StyleSheet.create({
     container: {
@@ -169,3 +256,39 @@ export default class EmailsFound extends Component {
     }
     
   })
+
+  const DATA_DUMMY = [
+    { id: 'emily-1',
+      name: {
+          brand: 'Patagonia',
+          style: 'P-6 Logo Tee',
+          full_name: 'Patagonia P-6 Logo Tee'
+      },
+      purchase_price: '$36.00',
+      purchase_date: '03/19/2016',
+      uri: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+    },
+
+    { id: 'emily-2',
+      name: {
+        brand: 'J.CREW',
+        style: 'Village Coat',
+        full_name: 'J.CREW Village Coat'
+    },
+    purchase_price: '$149.99',
+    purchase_date: '06/19/2016',
+    uri: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+      },
+
+      { id: 'emily-3',
+        name: {
+          brand: 'A&F',
+          style: 'Icon Hoodie',
+          full_name: 'A&F Icon Hoodie'
+      },
+      purchase_price: '$41.30',
+      purchase_date: '02/28/2017',
+      uri: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+      },
+    
+  ];
