d86a3a9c5ac2ef65693ed5f7d5bf9927e7e6cfb4
hweelin-yeo <hy384@cornell.edu>
2020-04-23 16:48:37 -0400

    emails found page sorta done

--- fabriq-app/constants/style.js
+++ fabriq-app/constants/style.js
@@ -1,6 +1,7 @@
 // - MARK: Colors
 export const LIGHTBLUE = '#3FBAFF';
-export const GREEN = 'green';
+export const GREEN = '#32a852';
+export const TURQUOISE = '#2ECFB4';
 
 // - MARK: Size
 export const BUTTON_FONT_SIZE = 16;
